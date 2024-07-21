57f14bb29329898e3cfa02480f8fc4e440f5ccd0
raymondWai <waimanho2765galois@gmail.com>
2024-07-20 20:23:59 -0400

    Init

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::fmt::{self, Display, Formatter};
+use ureq::serde_json;
+
+pub mod apis;
+pub mod newsdata_io;
+pub use apis::GetLatestNewsParams;
+pub use newsdata_io::NewsdataIO;
+
+pub type Json = serde_json::Value;
+pub type ApiResult<T> = Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    /// An Error returned by the API
+    ApiError(String),
+    /// An Error not related to the API
+    RequestError(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::ApiError(msg) => write!(f, "API error: {}", msg),
+            Error::RequestError(msg) => write!(f, "Request error: {}", msg),
+        }
+    }
+}
